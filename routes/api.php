77c383842c141eb68fecd6d8030deb12240e51d6
MAIT <faheem.akram@mait.ae>
2025-10-22 17:00:13 +0500

    change

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Tenant\Dashboard\EmployeeDashboardController;
 use App\Http\Controllers\Tenant\Employee\AttendanceController;
