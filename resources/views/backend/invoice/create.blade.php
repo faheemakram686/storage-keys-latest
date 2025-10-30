ea5ebb785cd9f5c19b7013234f857bb9b121f17b
MAIT <faheem.akram@mait.ae>
2025-10-30 15:52:56 +0500

    insurence change

--- resources/views/backend/invoice/create.blade.php
+++ resources/views/backend/invoice/create.blade.php
@@ -579,9 +579,12 @@
                         var html3 = '';
                         var i;
                         var c = 0;
-                        // var total = 0.00;
+                        const price = data.contract[0].estimate.unit_price;
+                        const discount = data.contract[0].estimate.term_length.discount_percentage;
+                        const discountedPrice = price - (price * discount / 100);
                         $('#invoiceItems').empty();
                         dr++;
+
                         $('input[name=invoice_value]').val();
                         var   html = '<tr id="row'+dr+'" class="dynamic-added">'+
                             '<td><input type="hidden" name="invoiceItems[id][]" placeholder="id" class="form-control id_list" value="' + data.contract[0].estimate.term_length.id + '" /><span>' + dr + '</span></td>'+
@@ -589,7 +592,7 @@
                             '<td><input type="text" name="invoiceItems[name][]" placeholder="Item Name" class="form-control name_list" value="' + data.contract[0].estimate.storageunit.storage_unit_name +' / '+  data.contract[0].estimate.term_length.title + '" /></td>'+
                             '<td><input type="number" name="invoiceItems[qty][]" placeholder="QTY" class="form-control qty_list" value="'+  data.contract[0].estimate.term_length.term_period + '" min="0"/></td>'+
                             '<td><input type="text" name="invoiceItems[unit][]" placeholder="Unit" class="form-control unit_list" value="'+  data.contract[0].estimate.term_length.title + '" /></td>'+
-                            '<td><input type="text" name="invoiceItems[amount][]" placeholder="Price" class="form-control amount_list" value="' + (data.contract[0].estimate.unit_price * 100 /  data.contract[0].estimate.term_length.discount_percentage ) + '" min="0.00" /></td>'+
+                            '<td><input type="text" name="invoiceItems[amount][]" placeholder="Price" class="form-control amount_list" value="' + discountedPrice + '" min="0.00" /></td>'+
                             '<td><input type="number" name="invoiceItems[total][]" placeholder="Total" class="form-control total" value="' + data.contract[0].estimate.unit_price + '" min="0.00" /></td>'+
                             '<td><button type="button" name="remove" id="'+dr+'" class="btn  btn-sm btn-danger btn_remove"><em class="icon ni ni-trash-empty-fill"></em></button></td>'+
                             '</tr>';
